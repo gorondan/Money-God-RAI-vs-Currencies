3c4254634b71c8e5bf2ae666d100433be25b14c0
gorondan <goron.dan@gmail.com>
2024-01-17 06:29:07 +0200

    added MarketPrice(mp); general improovments; added monthly outputs

--- ExRates.py
+++ ExRates.py
@@ -1,9 +1,6 @@
-from os import rename
 import pandas as pd
-
+import numpy as np
 ExRates = pd.read_json("ExRates.csv")
-# result_df = pd.read_json(result)
 ExRates_df = pd.DataFrame(1/ExRates).astype(object)
 
-ExRates_df.loc['UnixTimestamp'] = ExRates_df.columns.astype('int64') // 10**9
-ExRates_final_df = ExRates_df.loc[['UnixTimestamp','EUR','GBP','CHF','AUD','CAD', 'JPY', 'CNY']]
+ExRates_final_df = ExRates_df
--- readAPI.py
+++ readAPI.py
@@ -2,7 +2,7 @@ import requests
 import os
 
 # Fill in the start_date & end_date & symbols & separators accordingly 
-url = "https://api.apilayer.com/exchangerates_data/timeseries?start_date=2021-11-17&end_date=2022-11-17&symbols=EUR,GBP,CHF,AUD,CAD,JPY,CNY&base=USD&separators=' '"
+url = "https://api.apilayer.com/exchangerates_data/timeseries?start_date=2023-11-03&end_date=2023-12-09&symbols=EUR,GBP,CHF,AUD,CAD,JPY,CNY&base=USD&separators=' '"
 
 payload = {}
 headers= {
@@ -11,7 +11,7 @@ headers= {
 response = requests.request("GET", url, headers=headers, data = payload)
 status_code = response.status_code
 result = response.text
-with open("ExRates.csv","w") as f:
+with open("ExRates_nov2023.csv","w") as f:
   f.write(result)
 
 # When updating the ExRates csv file, only leave "rates" keyword's values in the file, delete the rest of the keywords and afferent data. Use EXAMPLE_ExRates_FORMAT as format excample
